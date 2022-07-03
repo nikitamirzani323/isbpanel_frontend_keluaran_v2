b817342e76cfe09e41c398be962b36c62840ae53
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-07-04 03:23:51 +0700

    Update ENV + BANNER TOP + PLACEHOLDER

--- src/components/banner_top.svelte
+++ src/components/banner_top.svelte
@@ -1,13 +1,20 @@
 <script>
+    import Placholder from "../components/placholder.svelte";
     export let listbanner = [];
 </script>
-<article class="hidden lg:flex flex-col w-full gap-2 mb-2">
-    {#each listbanner as rec}
-        {#if rec.banner_posisi == "TOP" && rec.banner_device == "WEB"}
-            <img class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
-        {/if}
-    {/each}
-</article>
+{#if listbanner != ""}
+    <article class="hidden lg:flex flex-col w-full gap-2 mb-2">
+        {#each listbanner as rec}
+            {#if rec.banner_posisi == "TOP" && rec.banner_device == "WEB"}
+                <img  class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
+            {/if}
+        {/each}
+    </article>
+{:else}
+    <article class="hidden lg:flex flex-col w-full gap-2 mb-2">
+        <Placholder total_placeholder=2 />
+    </article>
+{/if}
 <article class="flex lg:hidden flex-col w-full gap-2 mb-2">
     {#each listbanner as rec}
         {#if rec.banner_posisi == "TOP" && rec.banner_device == "MOBILE"}
--- src/components/placholder.svelte
+++ src/components/placholder.svelte
@@ -11,15 +11,15 @@
     }
 </script>
 <div class="animate-pulse flex space-x-4">
-    <div class="flex-1 space-y-6 py-1">
+    <div class="flex-1 space-y-5 py-1">
         {#each data as rec }
-        <div class="h-2 bg-slate-200 rounded"></div>
-        <div class="space-y-3">
-            <div class="grid grid-cols-3 gap-4">
-                <div class="h-2 bg-slate-200 rounded col-span-2"></div>
-                <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+        <div class="h-3 bg-base-100 rounded"></div>
+        <div class="space-y-2">
+            <div class="grid grid-cols-3 gap-2">
+                <div class="h-3 bg-base-100 rounded col-span-2"></div>
+                <div class="h-3 bg-base-100 rounded col-span-1"></div>
             </div>
-            <div class="h-2 bg-slate-200 rounded"></div>
+            <div class="h-3 bg-base-100 rounded"></div>
         </div>
         {/each}
     </div>
