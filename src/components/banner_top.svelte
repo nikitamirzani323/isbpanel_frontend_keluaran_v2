23190d602872631b4e6899b67f0491bbd96d8ae6
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-07-04 15:56:12 +0700

    Update FAVICON + BANNER : URL WEBSITE

--- src/components/banner_top.svelte
+++ src/components/banner_top.svelte
@@ -6,7 +6,9 @@
     <article class="hidden lg:flex flex-col w-full gap-2 mb-2">
         {#each listbanner as rec}
             {#if rec.banner_posisi == "TOP" && rec.banner_device == "WEB"}
-                <img  class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
+                <a href="{rec.banner_urlwebsite}" target="_blank">
+                    <img  class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
+                </a>
             {/if}
         {/each}
     </article>
@@ -18,7 +20,9 @@
 <article class="flex lg:hidden flex-col w-full gap-2 mb-2">
     {#each listbanner as rec}
         {#if rec.banner_posisi == "TOP" && rec.banner_device == "MOBILE"}
-            <img class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
+            <a href="{rec.banner_urlwebsite}" target="_blank">
+                <img class="w-full " src="{rec.banner_url}" alt="BANNER ISB88">
+            </a>
         {/if}
     {/each}
 </article>
