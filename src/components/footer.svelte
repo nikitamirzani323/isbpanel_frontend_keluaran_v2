680eb376d2cda7f14669c322e320f2f87926ac43
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-08-11 06:37:56 +0700

    Update Footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footer footer-center p-5 bg-base-200 text-base-content rounded">
     <div>
-      <p class="text-xs">Copyright © 2021 - SDSB RESULT. All Right Reserved.</p>
+      <p class="text-xs">Copyright © 2021 - SCBD RESULT. All Right Reserved.</p>
     </div>
 </footer>
